fae1c0ed1cb59f89231f4c2b4a80d530c94aa0a1
rrll3553 <liveoak35@gmail.com>
2021-07-12 11:35:03 -0700

    Added and moved purchase button to side

--- src/components/IndividualMoviePage/IndividualMoviePage.js
+++ src/components/IndividualMoviePage/IndividualMoviePage.js
@@ -8,22 +8,34 @@ export default function IndividualMoviePage() {
   return (
     <div className="individualMoviePage">
       <h8 className="intro">Movie</h8>
-      <div className="side">
-        <div className="column">
-          <div className="moviePoster"></div>
-          <div className="movieTitle">Movie Title</div>
-          <div className="rating">Rating</div>
-          <div className="movieDescription">
-              <p>Description: "shdfshgadgdsgdsafgdsgdsgadsgdsagdsgdsgadsgdsgadsgadsgdsagadsgsgadgadsgadsgagsdfshdfhdfshfhdsfhdfshfdhdfshdfhfshshdfhfhshdfhhsdLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+      <div className="column">
+        <div className="top">
+          <div className="left">
+            <div className="moviePoster"></div>
+            <div className="movieTitle">Movie Title</div>
+            <div className="rating">Rating</div>
           </div>
-          <div className="discussionSection">
-              <p>Discussion:</p>
+          <div className="right">
+            <Link to="create">
+              <button>Purchase</button>
+            </Link>
           </div>
         </div>
-        <div className="test">
-          <Link to="create">
-            <button>This is a button</button>
-          </Link>
+        <div className="movieDescription">
+          <p>
+            Description:
+            "shdfshgadgdsgdsafgdsgdsgadsgdsagdsgdsgadsgdsgadsgadsgdsagadsgsgadgadsgadsgagsdfshdfhdfshfhdsfhdfshfdhdfshdfhfshshdfhfhshdfhhsdLorem
+            ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat. Duis aute irure dolor in reprehenderit in
+            voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+            officia deserunt mollit anim id est laborum."
+          </p>
+        </div>
+        <div className="discussionSection">
+          <p>Discussion:</p>
         </div>
       </div>
     </div>
