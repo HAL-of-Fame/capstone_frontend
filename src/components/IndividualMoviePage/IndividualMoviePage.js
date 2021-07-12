48fce5ccb0b37cb1d78aeaa54b6cd6f5277b8cba
rrll3553 <liveoak35@gmail.com>
2021-07-12 10:49:34 -0700

    Description  text stays inside description box

--- src/components/IndividualMoviePage/IndividualMoviePage.js
+++ src/components/IndividualMoviePage/IndividualMoviePage.js
@@ -8,18 +8,21 @@ export default function IndividualMoviePage() {
   return (
     <div className="individualMoviePage">
       <h8 className="intro">Movie</h8>
-      <div className="test">
-      <Link to="create">
-        <button>This is a button</button>
-      </Link>
+      <div className="side">
+        <div className="column">
+          <div className="moviePoster"></div>
+          <div className="movieTitle">Movie Title</div>
+          <div className="rating">Rating</div>
+          <div className="movieDescription">
+              <p>Description: "shdfshgadgdsgdsafgdsgdsgadsgdsagdsgdsgadsgdsgadsgadsgdsagadsgsgadgadsgadsgagsdfshdfhdfshfhdsfhdfshfdhdfshdfhfshshdfhfhshdfhhsdLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+          </div>
+        </div>
+        <div className="test">
+          <Link to="create">
+            <button>This is a button</button>
+          </Link>
+        </div>
       </div>
-      <div className="column">
-        <div className="moviePoster"></div>
-        <div className="movieTitle">Movie Title</div>
-        <div className="rating">Rating</div>
-      </div>
-      <div className="movieDescription">Description: blah blah</div>
-
     </div>
   );
 }
