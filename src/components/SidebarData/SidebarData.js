ae56727c27af7dfec0fe5fce1de9437a4ebf7a49
ashanij123 <80354834+ashanij123@users.noreply.github.com>
2021-07-12 18:44:15 -0400

    SideBar Updates

---
+++ src/components/SidebarData/SidebarData.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+import * as IoIcons from 'react-icons/io';
+import * as MdIcons from "react-icons/md";
+
+export const SidebarData = [
+  {
+    title: 'Home',
+    path: '/',
+    icon: <AiIcons.AiFillHome />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'Genre',
+    path: '/genre',
+    icon: <IoIcons.IoIosPaper />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'Trending',
+    path: '/trending',
+    icon: <IoIcons.IoIosTrendingUp />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'New Releases',
+    path: '/newreleases',
+    icon: <MdIcons.MdFiberNew />,
+    cName: 'nav-text'
+  },
+];
