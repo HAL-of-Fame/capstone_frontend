a92635c0a3ea33ab52fbd598721dd761d62cd327
lriveracastro407 <lriveracastro407@berkeley.edu>
2021-07-12 09:53:49 -0700

    completed search bar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,19 +1,18 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import search from "../../logos/search.svg";
 // import logo from "../../assets/codepath.svg"
 // import person from "../../assets/person.svg"
 // import Twitter from "../Icons/Twitter"
 // import Instagram from "../Icons/Instagram"
 // import Facebook from "../Icons/Facebook"
-import "./Navbar.css"
+import "./Navbar.css";
 
-export default function Navbar(
-  {
-    user,
-    handleLogout,
-    searchInputValue,
-    handleOnSearchInputChange
-  }
-) {
+export default function Navbar({
+  user,
+  handleLogout,
+  searchInputValue,
+  handleOnSearchInputChange,
+}) {
   return (
     <nav className="Navbar">
       <div className="content">
@@ -29,60 +28,56 @@ export default function Navbar(
           <Facebook fill="var(--pure-white)" />
         </div> */}
 
-            <div className="search-bar">
-            <input
-              type="text"
-              name="search"
-              placeholder="Search"
-              value={searchInputValue}
-              onChange={handleOnSearchInputChange}
+        <div className="search-bar-btn">
+          <input
+            className="search-bar"
+            type="text"
+            name="search"
+            placeholder="Search"
+            value={searchInputValue}
+            onChange={handleOnSearchInputChange}
+          />
+          <div className="search-btn">
+            <img
+              className="pic"
+              src={search}
+              alt="search button"
+              width="39px"
+              height="37.85px"
             />
-            <i className="material-icons">search</i>
           </div>
+        </div>
         <ul className="links">
-          <li>
-            <Link to="/genre">Genre</Link>
-          </li>
-          <li>
-            <Link to="/trending">Trending</Link>
-          </li>
-          <li>
-            <Link to="/newreleases">New Releases</Link>
-          </li>
-          <li>
-            <Link to="/favorites">Favorites</Link>
-          </li>
           <div className="cart">
-              <Link to="/shopping-cart">
-                My Cart
-                <i className="material-icons">shopping_cart</i>
-              </Link>
-            </div>
+            <Link to="/shopping-cart">
+              My Cart
+              <i className="material-icons">shopping_cart</i>
+            </Link>
+          </div>
           <div className="auth">
-          {user?.email ? (
-            <>
-              <li>
-                <span>{user.email}</span>
-              </li>
+            {user?.email ? (
+              <>
+                <li>
+                  <span>{user.email}</span>
+                </li>
 
-              <li>
-                <span onClick={handleLogout}>Logout</span>
-              </li>
-            </>
-          ) : (
-            <>
-              <li>
-                <Link to="/login">Login</Link>
-              </li>
-              <li>
-                <Link to="/register">Sign Up</Link>
-              </li>
-            </>
-          )}
-            </div>
+                <li>
+                  <span onClick={handleLogout}>Logout</span>
+                </li>
+              </>
+            ) : (
+              <>
+                <li>
+                  <Link to="/login">Login</Link>
+                </li>
+                <li>
+                  <Link to="/register">Sign Up</Link>
+                </li>
+              </>
+            )}
+          </div>
         </ul>
       </div>
-      
     </nav>
-  )
+  );
 }
