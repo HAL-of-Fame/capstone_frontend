8b8ccbe1efb8412671f94c82a12549923fab56d0
ashanij123 <80354834+ashanij123@users.noreply.github.com>
2021-07-20 16:11:32 -0400

    post changes on the frontend

---
+++ src/components/Post/Post.js
@@ -0,0 +1,24 @@
+// import { Link } from "react-router-dom"
+// import Stars from "../Stars/Stars"
+// import { formatRating, formatDate } from "../../utils/format"
+import "./Post.css"
+
+export default function Post({ post, user }) {
+//   const userOwnsPost = user?.username && exercise?.user_id === user?.username
+
+  return (
+    <div className="Post">
+      <div className="ExerciseCard">
+        <div className="header1">
+          <p className="name">{post.title}</p>
+        </div>
+        <div className="stats">
+          <div className="stat">
+            <p>Duration</p>
+            <span>{post.text}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ src/components/PostForm/PostForm.js
@@ -0,0 +1,80 @@
+import { useState } from "react"
+// import axios from "axios"
+import apiClient from "../Services/apiClient"
+// import NotAllowed from "../NotAllowed/NotAllowed"
+import "./PostForm.css"
+ 
+ 
+export default function NewPost({ user, addPost }) {
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState(null)
+  const [form, setForm] = useState({
+    title: "",
+    text: "",
+  })
+ 
+  const handleOnInputChange = (event) => {
+    setForm((f) => ({ ...f, [event.target.name]: event.target.value }))
+  }
+ 
+  const handleOnSubmit = async () => {
+    setIsLoading(true)
+ 
+    const { data, error } = await apiClient.createNewPost({ title: form.title, text: form.text })
+    if (data) {
+      addPost(data.post)
+      setForm({ title: "", text: "" })
+    }
+    if (error) {
+      setError(error)
+    }
+ 
+    setIsLoading(false)
+  }
+  const renderForm = () => {
+    // if (!user?.email) {
+    //   return <NotAllowed />
+    // }
+    return (
+      <div className="form">
+        <div className="input-field">
+          <label htmlFor="title">Title</label>
+          <input
+            type="text"
+            name="title"
+            placeholder="Enter Title Here"
+            value={form.title}
+            onChange={handleOnInputChange}
+          />
+        </div>
+ 
+        <div className="input-field">
+          <label htmlFor="text">Text</label>
+          <input
+            type="text"
+            name="text"
+            placeholder="Enter Text Here"
+            value={form.text}
+            onChange={handleOnInputChange}
+          />
+        </div>
+
+        <button className="btn" disabled={isLoading} onClick={handleOnSubmit}>
+          {isLoading ? "Loading..." : "Submit"}
+        </button>
+      </div>
+    )
+  }
+ 
+  return (
+    <div className="NewPostForm">
+      <div className="card">
+        <h2>Create a new post</h2>
+ 
+        {Boolean(error) && <span className="error">{error}</span>}
+ 
+        {renderForm()}
+      </div>
+    </div>
+  )
+}
