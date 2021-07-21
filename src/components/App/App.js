5c89ce0a64cf080e448be285a39201954f0187fa
lriveracastro407 <lriveracastro407@berkeley.edu>
2021-07-21 11:45:45 -0700

    started connected frontend cart to backend

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,39 +6,64 @@ import Signup from "../Signup/Signup";
 import Login from "../Login/Login";
 import Home from "../Home/Home";
 import NotFound from "../NotFound/NotFound";
-import GenrePage from "../GenrePage/GenrePage"
-import Sidebar from "../Sidebar/Sidebar"
-import Footer from "../Footer/Footer"
+import GenrePage from "../GenrePage/GenrePage";
+import Sidebar from "../Sidebar/Sidebar";
+import Footer from "../Footer/Footer";
 import IndividualMoviePage from "../IndividualMoviePage/IndividualMoviePage";
 import apiClient from "../Services/apiClient";
 import SearchPage from "../SearchPage/SearchPage";
-import MerchStore from "../MerchStore/MerchStore"
-import PostForm from "../PostForm/PostForm"
-import ActionPage from "../ActionPage/ActionPage"
-import HorrorPage from "../HorrorPage/HorrorPage"
-import ComedyPage from "../ComedyPage/ComedyPage"
-import DramaPage from "../DramaPage/DramaPage"
-import ScienceFictionPage from "../ScienceFictionPage/ScienceFictionPage"
-import RomancePage from "../RomancePage/RomancePage"
-import data from '../../data'
-import ShoppingCart from "../ShoppingCart/ShoppingCart"
-import Orders from "../Orders/Orders"
+import MerchStore from "../MerchStore/MerchStore";
+import PostForm from "../PostForm/PostForm";
+import ActionPage from "../ActionPage/ActionPage";
+import HorrorPage from "../HorrorPage/HorrorPage";
+import ComedyPage from "../ComedyPage/ComedyPage";
+import DramaPage from "../DramaPage/DramaPage";
+import ScienceFictionPage from "../ScienceFictionPage/ScienceFictionPage";
+import RomancePage from "../RomancePage/RomancePage";
+import data from "../../data";
+import ShoppingCart from "../ShoppingCart/ShoppingCart";
+import Orders from "../Orders/Orders";
+import axios from "axios";
 
 export default function App() {
   const { products } = data;
-  const [user, setUser] = useState(null)
-  const [error, setError] = useState(null)
-  const [post, setPost] = useState([])
-  const [orders, setOrders] = useState([])
-  const [isFetching, setIsFetching] = useState(false)
-  const [activeCategory, setActiveCategory] = useState("All Categories")
-  const [searchInputValue, setSearchInputValue] = useState("")
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState(null);
+  const [post, setPost] = useState([]);
+  const [orders, setOrders] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
+  const [activeCategory, setActiveCategory] = useState("All Categories");
+  const [searchInputValue, setSearchInputValue] = useState("");
+  const [cartItems, setCartItems] = useState([]);
+  const [isCheckingOut, setIsCheckingOut] = useState(false);
 
   const handleOnSearchInputChange = (event) => {
-    setSearchInputValue(event.target.value)
-  }
-
-  const [cartItems, setCartItems] = useState([]);
+    setSearchInputValue(event.target.value);
+  };
+  const handleOnCheckout = async () => {
+    setIsCheckingOut(true);
+    console.log(cartItems);
+    try {
+      const res = await axios.post("http://localhost:3001/orders", {
+        order: cartItems,
+      });
+      console.log(res.data.order);
+      if (res?.data?.order) {
+        setOrders((o) => [...res.data.order, ...o]);
+        setIsCheckingOut(false);
+        setCartItems([]);
+        return res.data.order;
+      } else {
+        setError("Error checking out.");
+      }
+    } catch (err) {
+      console.log(err);
+      const message = err?.response?.data?.error?.message;
+      setError(message ?? String(err));
+    } finally {
+      setIsCheckingOut(false);
+    }
+  };
   const onAdd = (product) => {
     const exist = cartItems.find((x) => x.id === product.id);
     if (exist) {
@@ -79,8 +104,8 @@ export default function App() {
     }
   }, []);
   const addPost = (newPost) => {
-    setPost((oldPost) => [...oldPost, newPost])
-  }
+    setPost((oldPost) => [...oldPost, newPost]);
+  };
   // handles the logout
   const handleLogout = async () => {
     await apiClient.logoutUser();
@@ -89,10 +114,10 @@ export default function App() {
   };
 
   return (
-      <div className="App">
-        <BrowserRouter>
+    <div className="App">
+      <BrowserRouter>
         <Navbar user={user} setUser={setUser} handleLogout={handleLogout} />
-        <Sidebar/>
+        <Sidebar />
         <Routes>
           <Route path="/" element={<Home user={user} />} />
           <GenrePage path="/genre" />
@@ -108,9 +133,14 @@ export default function App() {
           />
           <Route
             path="/shopping-cart"
-            element={<ShoppingCart cartItems={cartItems}
-            onAdd={onAdd}
-            onRemove={onRemove} />}
+            element={
+              <ShoppingCart
+                cartItems={cartItems}
+                handleOnCheckout={handleOnCheckout}
+                onAdd={onAdd}
+                onRemove={onRemove}
+              />
+            }
           />
           <Route
             path="/orders"
@@ -130,42 +160,27 @@ export default function App() {
             }
           />
           <Route path="/search/:searchInputValue" element={<SearchPage />} />
-          <Route
-            path="/genre"
-            element={<GenrePage />}
-            />
-          <Route
-            path="/genre/action"
-            element={<ActionPage />}
-            />
-          <Route
-            path="/genre/comedy"
-            element={<ComedyPage />}
-            />
-          <Route
-            path="/genre/romance"
-            element={<RomancePage />}
-            />
-          <Route
-            path="/genre/drama"
-            element={<DramaPage />}
-            />
+          <Route path="/genre" element={<GenrePage />} />
+          <Route path="/genre/action" element={<ActionPage />} />
+          <Route path="/genre/comedy" element={<ComedyPage />} />
+          <Route path="/genre/romance" element={<RomancePage />} />
+          <Route path="/genre/drama" element={<DramaPage />} />
           <Route
             path="/genre/science-fiction"
             element={<ScienceFictionPage />}
-            />
+          />
+          <Route path="/genre/horror" element={<HorrorPage />} />
           <Route
-            path="/genre/horror"
-            element={<HorrorPage />}
-            />
-          <Route path="/genre/action/create" element={<PostForm user={user} post={post} addPost={addPost} />} />
-            <Route
+            path="/genre/action/create"
+            element={<PostForm user={user} post={post} addPost={addPost} />}
+          />
+          <Route
             path="/store"
-            element={<MerchStore products={products} onAdd={onAdd}/>}
-            />
+            element={<MerchStore products={products} onAdd={onAdd} />}
+          />
         </Routes>
         <Footer />
       </BrowserRouter>
     </div>
-        )
+  );
 }
